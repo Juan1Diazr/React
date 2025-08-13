0c2a2e41d3f7a10ab34fd2ae75854df54f682ad3
Juan1Diazr <juanestebandiaz018@gmail.com>
2025-08-13 08:27:15 -0500

    components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
-
+'use client';
+import { useState } from "react";
+import Boton from "../components/boton";
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const[mensaje,setMensaje]=useState('');
+  //FUNCION PARA MANEJAR EL BOTON}
+    const manejarClick= (textoBoton:string)=>{
+      setMensaje(`Has pulsado el boton ${textoBoton}`);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      if(textoBoton === 'Aceptar'){
+        console.log('Has pulsado Aceptar');
+        alert('Has pulsado Aceptar');
+      } 
+      else if(textoBoton === 'Cancelar'){
+        console.log('Has pulsado Cancelar');
+        alert('Has pulsado Cancelar');
+      }
+    };
+   return(
+    <main style={{textAlign: 'center' , padding:'20px'}} >
+      <h1>Pagina Principal</h1>
+      {mensaje &&  <p style={{color:'blue',fontSize:'18px'}}>{mensaje}</p>}
+      <div>
+       <Boton texto="Aceptar" onClick={manejarClick}/>  
+       <Boton texto="Cancelar"onClick={manejarClick}/>
+      </div>
+      
+
+    </main>
+
+
+   );
+
+
 }
+
+// import {useState} from "react";
+// import Saludo from "../components/first";
+// import Boton from "../components/boton";
+// export  default function Home() {  
+
+//   const [contador, setContador]= useState(0);
+//  // variables para el contador
+//   const incrementar = () => setContador(contador + 1);
+//   const decrementar =() =>setContador(contador - 1);
+//   const resetear = () => setContador(0);
+ 
+//   return(
+//     <main style={{textAling: 'center', padding: '50px'}} >
+//       <h1>contador Dinamico</h1>
+//       <h2>{contador}</h2>
+
+
+//       {/* Botones para incrementar, decrementar y resetear el contador */}
+//       <button onClick={incrementar}>Aumentar +</button>{''}
+//       <button onClick={decrementar}>decrementar </button>{''}
+//       <button onClick={resetear}>Reinciar</button>
+//     </main>
+
+
+//  );
+// }
---
+++ src/components/boton.jsx
@@ -0,0 +1,37 @@
+export default function Boton({texto,onClick}){
+    const handleClick = () => {
+        console.log('Texto del Boton',texto);
+        //que debe ejecutar la funcion al hacer click
+        if(onClick) onClick(texto);
+
+    };
+  
+    return(
+        <div className="p-8">
+            <button onClick={handleClick}
+            className="
+            px-6 py-3
+            bg-green-500
+            hover:bg-blue-700
+            active:bg-yellow-800
+            text-white font-medium
+            rounded-lg
+            shadow-lg hover:shadow-xl
+            transform hover:scale-105 active:scale-95
+            transition-all duration-200
+            border-2 border-blue-700
+            focus:outline-none focus:ring-4 focus:ring-blue-300
+
+            "
+            >
+                
+                
+                {texto}
+                </button>;
+        </div>
+    
+    
+
+
+  );
+}
